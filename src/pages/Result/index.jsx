f49bd2163aaa70ce9638f766b186839b58fa730f
ho <howardxsheng@gmail.com>
2026-01-11 15:23:51 +0800

    feat: get scores from route state

--- src/pages/Result/index.jsx
+++ src/pages/Result/index.jsx
@@ -1,6 +1,21 @@
-
+import { useLocation, useNavigate } from "react-router-dom";
 import ResultView from "./ResultView";
 
-export default function ResultPage(props) {
-  return <ResultView {...props} />;
+export default function ResultPage({ data }) {
+  const navigate = useNavigate();
+  const { state } = useLocation();
+  const scores = state?.scores;
+
+  if (!scores) {
+    navigate("/question", { replace: true });
+    return null;
+  }
+
+  return (
+    <ResultView
+      data={data}
+      scores={scores}
+      onBackToHome={() => navigate("/")}
+    />
+  );
 }
